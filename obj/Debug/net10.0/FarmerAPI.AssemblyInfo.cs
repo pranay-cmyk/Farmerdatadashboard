9a07cde8ba8c02f6c0586b7aa981b4878c776533
ettapranay <ettapranayreddy@gmail.com>
2026-05-20 15:27:16 +0530

    Initial Commit May 20

--- obj/Debug/net10.0/FarmerAPI.AssemblyInfo.cs
+++ obj/Debug/net10.0/FarmerAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FarmerAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9966c06a7892ffdc2f0ea29448c777166811e64b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aae88a2e093e091747e234443699d6df1c967d5d")]
 [assembly: System.Reflection.AssemblyProductAttribute("FarmerAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FarmerAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
